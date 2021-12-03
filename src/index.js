824c826c9f53ef9c626e071f4908a94f9f68897f
pasindumarasinghe <e17207@eng.pdn.ac.lk>
2021-12-03 21:17:47 +0530

    Implement data fetching functionality

---
+++ src/index.js
@@ -0,0 +1,22 @@
+/*Fetch necessary data from https://hpb.health.gov.lk */
+
+import axios from "axios";
+
+api = "https://hpb.health.gov.lk/api/get-current-statistical"
+
+const last_updated = document.getElementById("last-updated");
+const new_cases = document.getElementById("new-cases");
+const deaths = document.getElementById("deaths");
+const error = document.getElementById("error");
+
+//fetch the data and put them in proper places
+const fetchData = async () => {
+    try {
+        const response = await axios.get(api);
+        last_updated.textContent = response.data.update_date_time;
+        new_cases.textContent = response.data.local_new_cases;
+        deaths.textContent = response.data.local_new_deaths;
+    } catch (e) {
+        error.textContent = "Error";
+    }
+}
